447f77e62ee41ff162248b9c2ed98d299e6e9b2e
DiamondSssssss <trongtri140604@gmail.com>
2024-10-10 10:32:20 +0700

    Initial commit

---
+++ src/test/java/com/familyapp/application/FamilyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familyapp.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
