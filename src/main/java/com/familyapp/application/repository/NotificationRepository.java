3ad50ae6d2d1ef403c7e37506bc6fabfe396597a
DiamondSssssss <trongtri140604@gmail.com>
2024-10-10 21:10:00 +0700

    repository

---
+++ src/main/java/com/familyapp/application/repository/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.familyapp.application.repository;
+
+import com.familyapp.application.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
---
+++ src/main/java/com/familyapp/application/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.familyapp.application.repository;
+
+import com.familyapp.application.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
